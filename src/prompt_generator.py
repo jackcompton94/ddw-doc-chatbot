202e00c6239d13a36a184c067360c86f28ac1d09
jackcompton94 <jackcompton94@gmail.com>
2023-08-25 22:34:22 -0500

    revised prompt generator flow

--- src/prompt_generator.py
+++ src/prompt_generator.py
@@ -10,24 +10,22 @@ COMMAND_KEYWORDS = ["send", "give", "show", "display",
                     "walk me through", "guide", "help with",
                     "perform", "execute"]
 
-MISC_INTENTS = ["unclear", "no intent", "no intent detected",
-                "greeting", "introduction", "identification", "identity",
-                "about bot", "explaining capabilities", "clarify intent",
-                "request for assistance", "seeking clarification", "introduction request"]
+MISC_INTENTS = ["greeting", "introduction", "identification", "identity",
+                "bot information", "bot identity", "about bot", "capabilities"]
 
 
-def generate_prompt(question, intention, max_similarity, best_title, best_response, best_url):
+def generate_prompt(question, intention, max_similarity, best_title, best_content, best_url):
     confidence_threshold = 0.775
 
     # Irrelevant intent will engage with the user and encourage a question about data.world
     if any(keyword in intention.lower() for keyword in MISC_INTENTS):
         return \
-            f"INSTRUCTIONS: You are a support bot for data.world. Engage in a friendly conversation and respond to the user. Let them know that you would like to help them understand the platform.\n" \
+            f"INSTRUCTIONS: You are a support bot for data.world. Engage in a friendly conversation and respond to the user. Let them know that you are here to help them understand the platform.\n" \
             f"QUESTION: {question}\n" \
             f"RESPONSE:"
 
     # Check if AI is confident in its response
-    elif max_similarity < confidence_threshold:
+    elif max_similarity < confidence_threshold or intention.lower() == 'unclear':
 
         # Low confidence suggests to check the docs directly and reach out to the support team
         return f"INSTRUCTIONS: You are a support bot for data.world. Engage in a friendly conversation but for this particular question, recommend the user to reword their prompt or reach out to the support team for personalized assistance: https://support.data.world\n" \
@@ -37,24 +35,24 @@ def generate_prompt(question, intention, max_similarity, best_title, best_respon
     # Check if the user's input contains keywords indicating a question
     elif any(keyword in question.lower() for keyword in QUESTION_KEYWORDS):
         return \
-            f"DOCUMENTATION: {best_title} {best_response}\n URL:{best_url}\n" \
-            f"INSTRUCTIONS: You are a support bot for data.world. Review the intention and question below, and support your response with relevant information from the documentation above by providing helpful guidance to the user. Finally, provide the URL for reference.\n" \
+            f"DOCUMENTATION: {best_title} {best_content}\n URL:{best_url}\n" \
+            f"INSTRUCTIONS: You are a support bot for data.world. Provide a comprehensive answer to the users question. Utilize the documentation above and deliver a thorough response, make sure to address all aspects of the user's query. Include relevant examples, details, and explanations from the provided documentation and provide the URL.\n" \
             f"INTENTION: {intention}\n QUESTION: {question}\n" \
             f"RESPONSE:"
 
     # Check if the user's input contains keywords indicating a command
     elif any(keyword in question.lower() for keyword in COMMAND_KEYWORDS):
         return \
-            f"DOCUMENTATION: {best_title} {best_response}\n URL:{best_url}\n" \
-            f"INSTRUCTIONS: You are a support bot for data.world. Review the intention and question below, and engage in a friendly conversation by offer assistance when addressing the question below by using the documentation above to support your response. Finally, provide the URL for reference.\n" \
+            f"DOCUMENTATION: {best_title} {best_content}\n URL:{best_url}\n" \
+            f"INSTRUCTIONS: You are a support bot for data.world. Your goal is to assist users effectively when they issue commands or request actions. Engage in a helpful conversation, provide guidance, and explain how to execute the requested action using the documentation provided and provide the URL.\n" \
             f"INTENTION: {intention}\n QUESTION: {question}\n" \
             f"RESPONSE:"
 
     # Provide a conversational response
     else:
         return \
-            f"DOCUMENTATION: {best_title} {best_response}\n URL:{best_url}\n" \
-            f"INSTRUCTIONS: You are a support bot for data.world. Review the intention below and provide a helpful and engaging response, use the documentation above and provide the link.\n" \
+            f"DOCUMENTATION: {best_title} {best_content}\n URL:{best_url}\n" \
+            f"INSTRUCTIONS: You are a support bot for data.world. Your aim is to provide friendly and informative responses that promote a positive user experience. Engage in a conversational manner, incorporating insights from the provided documentation to enrich your answers and provide the URL.\n" \
             f"INTENTION: {intention}\n QUESTION: {question}\n" \
             f"RESPONSE:"
 
